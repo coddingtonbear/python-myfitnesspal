276fa9f3da9c2f14c4a3da403c1584963e6d92a1
Adam Coddington <adam.coddington@coxinc.com>
2014-08-12 20:22:43 -0700

    s/latestrevision/coddingtonbear/g

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as in_:
 setup(
     name='myfitnesspal',
     version='1.2.2',
-    url='http://github.com/latestrevision/python-myfitnesspal/',
+    url='http://github.com/coddingtonbear/python-myfitnesspal/',
     description='Access health and fitness data stored in Myfitnesspal',
     author='Adam Coddington',
     author_email='me@adamcoddington.net',
