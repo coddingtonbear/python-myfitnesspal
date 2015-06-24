6730322836dc2254b7bc970a18b5f348268e3529
Ian Ozsvald <ian@ianozsvald.com>
2015-06-24 21:39:46 +0100

    added note

--- myfitnesspal/base.py
+++ myfitnesspal/base.py
@@ -1,6 +1,7 @@
 class MFPBase(object):
     def __str__(self):
         #return self.__unicode__().encode('ascii', 'replace')
+        # return unicode for both Python 2.7 and 3.4 PORTING_CHECK
         return self.__unicode__()
 
     def __repr__(self):
