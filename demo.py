d03abbf45334d7edc21e7f8deb8cb72d3f3babb4
Ian Ozsvald <ian@ianozsvald.com>
2015-06-24 21:58:12 +0100

    fetch 7 days of food

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from datetime import datetime as dt
+from datetime import timedelta as td
 import argparse
 import myfitnesspal
 
@@ -25,3 +26,12 @@ if __name__ == "__main__":
     print("Water", day.water)
     print("Notes", day.notes)
     print("Keys for today", day.keys())
+
+    print("----")
+    for day_offset in range(7):
+        td_day = td(hours=24*day_offset)
+        a_day = today - td_day
+        day = client.get_date(a_day)
+        print("Fetching", a_day)
+        print("Day", day)
+
