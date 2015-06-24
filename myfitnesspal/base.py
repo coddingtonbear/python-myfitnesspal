e5044952531fbaf0d3a7296b0da59bc8869f119e
Ian Ozsvald <ian@ianozsvald.com>
2015-06-24 18:11:32 +0100

    no need to decode text here, it is already unicode

--- myfitnesspal/base.py
+++ myfitnesspal/base.py
@@ -1,6 +1,7 @@
 class MFPBase(object):
     def __str__(self):
-        return self.__unicode__().encode('ascii', 'replace')
+        #return self.__unicode__().encode('ascii', 'replace')
+        return self.__unicode__()
 
     def __repr__(self):
         return '<%s>' % (
