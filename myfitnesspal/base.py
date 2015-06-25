3c3c1e11750005ff253a4edf1c3b83ac14a38a0a
Ian Ozsvald <ian@ianozsvald.com>
2015-06-25 09:53:23 +0100

    hacky solution using six to check for the right way to print unicode encoded meal names and notes using py2 and py3

--- myfitnesspal/base.py
+++ myfitnesspal/base.py
@@ -1,8 +1,13 @@
+import six
+
 class MFPBase(object):
     def __str__(self):
         #return self.__unicode__().encode('ascii', 'replace')
         # return unicode for both Python 2.7 and 3.4 PORTING_CHECK
-        return self.__unicode__()
+        if six.PY2:
+            return self.__unicode__().encode('utf8', 'replace')
+        else:
+            return self.__unicode__()
 
     def __repr__(self):
         return '<%s>' % (
