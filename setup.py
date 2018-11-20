7afa5c2625c52eae92a9412eb6683c13efa202c4
Adam Coddington <me@adamcoddington.net>
2018-11-20 13:12:17 -0800

    Bump version: 1.13.1 → 1.13.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as in_:
 
 setup(
     name='myfitnesspal',
-    version='1.13.1',
+    version='1.13.2',
     url='http://github.com/coddingtonbear/python-myfitnesspal/',
     description='Access health and fitness data stored in Myfitnesspal',
     author='Adam Coddington',
