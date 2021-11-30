3329122420f772d0ccb97a8d3ec8bd6b5b097a2f
Adam Coddington <me@adamcoddington.net>
2021-11-30 07:05:35 -0800

    Bump version: 1.16.4 → 1.16.5

--- myfitnesspal/__init__.py
+++ myfitnesspal/__init__.py
@@ -1,5 +1,5 @@
 from myfitnesspal.client import Client  # noqa
 
-__version__ = "1.16.4"
+__version__ = "1.16.5"
 
 VERSION = tuple(int(v) for v in __version__.split("."))
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as in_:
 
 setup(
     name="myfitnesspal",
-    version="1.16.4",
+    version="1.16.5",
     url="http://github.com/coddingtonbear/python-myfitnesspal/",
     description="Access health and fitness data stored in Myfitnesspal",
     author="Adam Coddington",
